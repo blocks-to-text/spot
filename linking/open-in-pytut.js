2e665009643882951e6a9380ef3d8381f5f2760f
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2019-04-23 23:48:26 +0200

    Update open-in-pytut.js

--- linking/open-in-pytut.js
+++ linking/open-in-pytut.js
@@ -1,13 +1,8 @@
 function open_in_pytut() {
-  var code;
-  if (mode === "change") {
-    code = editor.getValue();
-  } else {
-    code = snippet;
-  };
+  var code = editor.getValue();
   var encoded = encodeURIComponent(code);
   var sanitized = encoded.replace(/\(/g, '%28').replace(/\)/g, '%29');
   var de_tabbed = sanitized.replace(/%09/g, '%20%20');
-  var url = "http://www.pythontutor.com/visualize.html#code="+sanitized+"&curInstr=0&mode=display";
-  window.open(url, '_self');
+  var url = "http://www.pythontutor.com/visualize.html#code="+sanitized+"&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false";
+  window.open(url, '_blank');
 };
